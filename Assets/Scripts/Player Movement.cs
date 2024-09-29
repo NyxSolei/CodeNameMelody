c9e45452257df20b30b4db99eb2273bd93e5e745
FlowerFacer <brits.abstract123@gmail.com>
2024-09-29 23:56:46 +0300

    First steps

---
+++ Assets/Scripts/Player Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); 
+    }
+
+}
