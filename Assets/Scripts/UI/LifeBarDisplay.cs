4bae987725dd0fe388f4638b69281ad173fdf12e
NyxSolei <natashasolei@gmail.com>
2024-10-20 17:02:34 +0300

    LifeBar Display script +UI

--- Assets/Scripts/UI/LifeBarDisplay.cs
+++ Assets/Scripts/UI/LifeBarDisplay.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class LifeBarDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Slider _healthSlider;
+
+    public static LifeBarDisplay instance;
+    void Awake()
     {
-        
+        // Set the static instance
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void UpdateHealthBar()
+    {
+        this._healthSlider.value = PlayerControls.instance.GetHealth();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitializeHealthBar()
     {
-        
+        this._healthSlider.value = PlayerControls.instance.GetStartingHealth();
+        this.SetMaxHealthBarDisplay();
+    }
+
+    public void SetMaxHealthBarDisplay()
+    {
+        this._healthSlider.maxValue = PlayerControls.instance.GetStartingHealth();
     }
 }
